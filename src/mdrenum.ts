33767828b344656d44f0e7d72f5c4f33e4f8bb1e
David Eisinger <david.eisinger@gmail.com>
2023-11-10 19:57:41 -0500

    Working TS solution

--- src/mdrenum.ts
+++ src/mdrenum.ts
@@ -1,58 +1,118 @@
-import fs from 'node:fs/promises'
+import fs from 'node:fs'
+import {globSync} from 'glob'
 import {fromMarkdown} from 'mdast-util-from-markdown'
-import {toMarkdown} from 'mdast-util-to-markdown'
 import {Node, Parent, LinkReference, Definition} from 'mdast'
 
-const doc = await fs.readFile('test.md')
-const tree = fromMarkdown(doc)
-const content = doc.toString()
+interface RefMap {
+  [index: string]: number
+}
 
-let findLinks = function(node: Node): LinkReference[] {
-  if (node.type === 'linkReference') {
-    return [node as LinkReference]
+type LinkNode = LinkReference | Definition
+
+function findNodes(node: Node): LinkNode[] {
+  if (node.type === 'linkReference' || node.type === 'definition') {
+    const linkNode = node as LinkNode
+
+    if (linkNode.identifier.match(/^\d+$/)) {
+      return [linkNode]
+    }
   }
 
   if ('children' in node) {
     const parent = node as Parent
 
     return parent.children.reduce(
-      function(links, child) { return links.concat(findLinks(child)) },
-      [] as LinkReference[]
+      function(links, child) { return links.concat(findNodes(child)) },
+      [] as LinkNode[]
     )
   }
 
   return []
 }
 
-let findDefinitions = function(node: Node): Definition[] {
-  if (node.type === 'definition') {
-    return [node as Definition]
-  }
+function buildRefMap(nodes: LinkNode[]): RefMap {
+  let index = 1
 
-  if ('children' in node) {
-    const parent = node as Parent
+  return nodes.reduce(
+    function (dict, link) {
+      if (!dict[link.identifier]) {
+        dict[link.identifier] = index
+        index++
+      }
 
-    return parent.children.reduce(
-      function(links, child) { return links.concat(findDefinitions(child)) },
-      [] as Definition[]
-    )
-  }
-
-  return []
+      return dict
+    },
+    {}
+  )
 }
 
-findLinks(tree).forEach(function(link) {
-  console.log(link)
+function updateContent(nodes: LinkNode[], refMap: RefMap, content: string): string {
+  let offset = 0
 
-  if (link.position && link.position.start.offset && link.position.end.offset) {
-    console.log(content.substring(link.position.start.offset, link.position.end.offset))
+  return nodes.reduce(
+    function(str, node) {
+      if (node.position == undefined) {
+        return str
+      }
+
+      let start = node.position.start.offset
+      let end = node.position.end.offset
+
+      if (start == undefined || end == undefined) {
+        return str
+      }
+
+      start += offset
+      end += offset
+
+      let nodeContent = str.substring(start, end)
+      let matcher = new RegExp(`\\[${node.identifier}\\]$`)
+
+      if (node.type == "definition") {
+        matcher = new RegExp(`^\\[${node.identifier}\\]`)
+      }
+
+      let updatedContent = nodeContent.replace(matcher, `[${refMap[node.identifier]}]`)
+
+      offset += updatedContent.length - nodeContent.length
+
+      return str.substring(0, start) + updatedContent + str.substring(end)
+    },
+    content
+  )
+}
+
+let files = process.argv.slice(2)
+let fix = false
+let failed = false
+
+if (files[0] === "--fix") {
+  fix = true
+  files.shift()
+}
+
+if (files.length === 0) {
+  files = globSync('**/*.md')
+}
+
+files.forEach(function(file) {
+  const content= fs.readFileSync(file).toString()
+  const tree = fromMarkdown(content)
+
+  const nodes = findNodes(tree)
+  const refMap = buildRefMap(nodes)
+  const updated = updateContent(nodes, refMap, content)
+
+  if (content !== updated) {
+    console.error(`Links in ${file} are not in order`)
+    failed = true
+
+    if (fix) {
+      fs.writeFileSync(file, updated)
+    }
   }
 })
 
-findDefinitions(tree).forEach(function(definition) {
-  console.log(definition)
-
-  if (definition.position && definition.position.start.offset && definition.position.end.offset) {
-    console.log(content.substring(definition.position.start.offset, definition.position.end.offset))
-  }
-})
+if (failed) {
+  process.exit(1)
+}
